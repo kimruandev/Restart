a86921090932cc52e1b08c0803aadca12ee88c4c
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-11 11:20:23 -0300

    Project initial structure

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,36 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Kim Ruan on 11/09/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
